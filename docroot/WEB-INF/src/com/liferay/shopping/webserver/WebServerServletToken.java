b119ec220f2437ffd1a98ecebd4490c117badfa1
Alvaro del Castillo <acs@barrapunto.com>
2011-10-03 06:34:20 +0200

    Need files for 6.0.6 version

---
+++ docroot/WEB-INF/src/com/liferay/shopping/webserver/WebServerServletToken.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.shopping.webserver;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public interface WebServerServletToken {
+
+	public String getToken(long imageId);
+
+	public void resetToken(long imageId);
+
+}
---
+++ docroot/WEB-INF/src/com/liferay/shopping/webserver/WebServerServletTokenUtil.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.shopping.webserver;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+public class WebServerServletTokenUtil {
+
+	public static WebServerServletToken getWebServerServletToken() {
+		return _webServerServletToken;
+	}
+
+	public static String getToken(long imageId) {
+		return getWebServerServletToken().getToken(imageId);
+	}
+
+	public static void resetToken(long imageId) {
+		getWebServerServletToken().resetToken(imageId);
+	}
+
+	public void setWebServerServletToken(
+		WebServerServletToken webServerServletToken) {
+
+		_webServerServletToken = webServerServletToken;
+	}
+
+	private static WebServerServletToken _webServerServletToken;
+
+}
